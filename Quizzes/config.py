0491ef8eb5ae0b1828e04111181347e5e77ad790
Teja Tanush <tejatanush47@gmail.com>
2025-07-07 23:48:28 +0530

    quiz with files done

--- Quizzes/config.py
+++ Quizzes/config.py
@@ -3,5 +3,6 @@ from dotenv import load_dotenv
 def get_env():
     load_dotenv()
     GROQ_API_KEY=os.getenv("GROQ_API_KEY")
-    return GROQ_API_KEY
-GROQ_API_KEY=get_env()
+    google_api_key = os.getenv("GOOGLE_GENAI_API_KEY")
+    return GROQ_API_KEY,google_api_key
+GROQ_API_KEY,google_Api_key=get_env()
